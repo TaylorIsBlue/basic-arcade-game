2ea443ee56421409e1af83f5f3977fdadbd2cc87
Taylor <59410102+TaylorIsBlue@users.noreply.github.com>
2021-09-15 21:32:12 -0700

    fixed whatever i did

--- images.g.ts
+++ images.g.ts
@@ -5,39 +5,39 @@ namespace myImages {
         switch(helpers.stringTrim(name)) {
             case "image1":
             case "h":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-. . . . f . . . . . f . . . . . 
-. . . . f . . . . . f . . . . . 
-. . . . f . . . . . f . . . . . 
-. . . . f . . . . . f . . . . . 
-. . . . f . . . . . f . . . . . 
-. . . . . f f f f f . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . 1 . . f . . 1 . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
 `;
             case "image2":
             case "pizza":return img`
 . . . . . . . . . . . . . . . . 
-. . . . e e e e e e e . . . . . 
-. . . e d d d d d d d e . . . . 
-. . e d d d d d d d d d e . . . 
-. e d d d 2 d d d 2 d d d e . . 
-. e d d d d d d d d d d d e . . 
-. e d 2 d d d 2 d d d 2 d e . . 
-. e d d d d d d d d d d d e . . 
-. e d d d 2 d d d 2 d d d e . . 
-. e d d d d d d d d d d d e . . 
-. e d 2 d d d d d d 2 d d e . . 
-. . e d d d d d d d d d e . . . 
-. . . e d d d d d d d e . . . . 
-. . . . e e e e e e e . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f d d d d d d d f . . . . 
+. . f d d d d d d d d d f . . . 
+. f d d d 2 d d d 2 d d d f . . 
+. f d d d d d d d d d d d f . . 
+. f d 2 d d d 2 d d d 2 d f . . 
+. f d d d d d d d d d d d f . . 
+. f f d d 2 d d d 2 d d f f . . 
+. f d f d d d d d d d f d f . . 
+. f d d f f f f f f f d d f . . 
+. . f d d d d d d d d d f . . . 
+. . . f d d d d d d d f . . . . 
+. . . . f f f f f f f . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
+let cooldown = 10
+let mySprite =sprites.create(assets.image`h`, SpriteKind.Player)
+let cake = sprites.create(assets.image`pizza`, SpriteKind.Food)
+controller.moveSprite(mySprite)
+scene.setBackgroundColor(7)
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, other) {
     info.changeScoreBy(1)
-    music.baDing.play()
+    music.powerUp.play(music.volume())
+    if (info.score() == 15) scene.cameraShake()
     other.setPosition(randint(0, 160), randint(0, 120))
-    info.startCountdown(5)
+    info.startCountdown(cooldown)
 })
 
-scene.setBackgroundColor(7)
-let mySprite = sprites.create(assets.image`h`, SpriteKind.Player)
-controller.moveSprite(mySprite)
-let cake = sprites.create(assets.image`pizza`, SpriteKind.Food)
-let cooldown = 10
 
-if(info.score() == 15) {
-    scene.cameraShake()
-}
+
+
+
