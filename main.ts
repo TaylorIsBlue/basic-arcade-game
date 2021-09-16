707d90ecddf0ce9103b0366f6cacdbb7fea57e7c
Taylor <59410102+TaylorIsBlue@users.noreply.github.com>
2021-09-15 20:56:30 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, other) {
     info.changeScoreBy(1)
+    music.baDing.play()
     other.setPosition(randint(0, 160), randint(0, 120))
     info.startCountdown(5)
 })
@@ -8,5 +9,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, other) {
 scene.setBackgroundColor(7)
 let mySprite = sprites.create(assets.image`h`, SpriteKind.Player)
 controller.moveSprite(mySprite)
-let pizza = sprites.create(assets.image`pizza`, SpriteKind.Food)
+let cake = sprites.create(assets.image`pizza`, SpriteKind.Food)
 let cooldown = 10
+
+if(info.score() == 15) {
+    scene.cameraShake()
+}
